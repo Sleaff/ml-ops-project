7a0ff215a3546108b7e73db3b1c1c9b533f9012e
Sleaf <wowkenneth@gmail.com>
2026-01-15 13:48:35 +0100

    test

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,12 +1,11 @@
-### Dataset is not on git and therefore always fails
-'''
 from project.dataset import NewsDataset
 from torch.utils.data import Dataset
 from transformers import AutoTokenizer
 
+
 def test_news_dataset():
     """Test the NewsDataset class."""
+
     tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
     dataset = NewsDataset("data/processed/news.csv", tokenizer)
     assert isinstance(dataset, Dataset)
-'''
